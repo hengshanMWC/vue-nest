23883700e8116f3cd5ccc42016d1237d8c64073e
mawencan <1819293933@qq.com>
2023-10-12 23:12:44 +0800

    feat(log): 引入@abmao/nest-logs
    做日志监控

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { NestLogsModule } from '@abmao/nest-logs';
 @Module({
-  imports: [],
+  imports: [NestLogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
