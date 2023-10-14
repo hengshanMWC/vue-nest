1bafbc578c28aa6d7faa7a9557394b74651393da
mawencan <1819293933@qq.com>
2023-10-14 22:58:24 +0800

    fix(assets): logo组件路径修正

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import Logo from '@/assets/logo.svg'
+import Logo from '@/assets/images/logo.svg'
 </script>
 
 <template>
