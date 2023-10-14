3e3fa79b513930df27d345abb073fd22f945f61a
mawencan <1819293933@qq.com>
2023-10-14 17:08:59 +0800

    build(client): vite构建优化,添加了env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import svgLoader from 'vite-svg-loader'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,10 +13,29 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    /** 将 SVG 静态图转化为 Vue 组件 */
+    svgLoader(),
+    // 需自动加载组件
+    AutoImport({
+      resolvers: [NaiveUiResolver()]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8081',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
 })
