2d08e1dca7e8194dbaca26f6fcd2cde1994472a7
mawencan <1819293933@qq.com>
2023-10-15 11:09:56 +0800

    feat(css): 关于 tailwind 的 preflight 样式
    你可能会发现在静态 HTML 文件中加入 meta 标签没用（naive 的样式仍然可能被覆盖），因为你的工具链可能永远会把 tailwind 的样式插入 head 的尾部。这种情况下，你需要在
    app 挂载之前动态的插入 meta 标签。

--- src/main.ts
+++ src/main.ts
@@ -14,4 +14,9 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+// 关于 tailwind 的 preflight 样式
+const meta = document.createElement('meta')
+meta.name = 'naive-ui-style'
+document.head.appendChild(meta)
+
 app.mount('#app')
